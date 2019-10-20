68e3574a0677f1ef30e02969fd22425a3e404dba
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-20 12:29:33 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 function writeCards(name, event){
   
-  let thankYouCard=[];
+  let thankYouCard = [];
   for(i = 0; i<name.length;i++);
   thankYouCard.push(`thank you ${name[i]}, for the wonderful ${event} gift!`);
-   return thankYouCard;
+   
 }
 
-
+return thankYouCard;
 
 
 function countdown(i){
-  while(i>0);{
+  while(i>0){
     console.log(i);
     i--;
   }
@@ -20,28 +20,3 @@ function countdown(i){
 }
 countdown(10);
 
-
-
-
-
-// function writeCards(name, event) {
-
-//       let thankYouCard = [] /
-//     for (let i = 0; i < name.length; i++) {
-    
-//     thankYouCard.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
-//   }
-//   return thankYouCard; 
-
-
-
-
-// function countdown(i){
-//   while ( i > 0){
-//     console.log(i); 
-//     i--;
-//   }
-//   console.log(i);
-// }
-
-// countdown(10);
