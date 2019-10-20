db7645f65830754561ea360f4ae18da9c9f8b6cd
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-20 12:49:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Code your solutions in this file
 
-function writeCards(name, event){
-  
-  let thankYouCard = [];
-  for(i = 0; i<name.length;i++);
-  thankYouCard.push(`thank you ${name[i]}, for the wonderful ${event} gift!`);
-   
+function writeCards(name , event) {
+
+      let thankYouCard = [] 
+     for (let i = 0; i < name.length; i++) {
+    thankYouCard.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCard; 
 }
 
-return thankYouCard;
 
 
 function countdown(i){
-  while(i>0){
-    console.log(i);
+  while ( i > 0){
+    console.log(i); 
     i--;
   }
-  console.log(i);
+  console.log(i); 
 }
-countdown(10);
 
+countdown(10);
