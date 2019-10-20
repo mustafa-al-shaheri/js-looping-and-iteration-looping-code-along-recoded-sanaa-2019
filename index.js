53996ab700334ef22adf6bdba88d73c32201e7db
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-20 12:21:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+  
+  let thankYouCard=[];
+  for(i = 0; i<name.length;i++);
+  thankYouCard.push(`thank you ${name[i]}, for the wonderful ${event} gift!`);
+   return thankYouCard;
+}
+
+
+
+
+function countdown(i){
+  while(i>0);{
+    console.log(i);
+    i--;
+  }
+  console.log(i);
+}
+countdown(10);
+
+
+
+
+
+// function writeCards(name, event) {
+
+//       let thankYouCard = [] /
+//     for (let i = 0; i < name.length; i++) {
+    
+//     thankYouCard.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+//   }
+//   return thankYouCard; 
+
+
+
+
+// function countdown(i){
+//   while ( i > 0){
+//     console.log(i); 
+//     i--;
+//   }
+//   console.log(i);
+// }
+
+// countdown(10);
