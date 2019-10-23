2cb0b941a0768756d08386322599e8532d45a840
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-23 20:34:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 
+<<<<<<< HEAD
 function writeCards(name , event) {
 
       let thankYouCard = [] 
@@ -10,6 +11,19 @@ function writeCards(name , event) {
 }
 
 
+=======
+function writeCards(name, event){
+<<<<<<< HEAD
+  
+  let thankYouCard = [];
+  for(let i = 0; i<name.length;i++){
+  thankYouCard.push(`thank you ${name[i]}, for the wonderful ${event} gift!`);
+   
+}
+
+return thankYouCard;
+}
+>>>>>>> 292c2b7471c575d018d7ed647435c354adabdc6d
 
 function countdown(i){
   while ( i > 0){
@@ -19,4 +33,13 @@ function countdown(i){
   console.log(i); 
 }
 
+<<<<<<< HEAD
 countdown(10);
+=======
+=======
+  for (let i = 0; i < writeCards.length; i++){
+    console.log(`Thank you ${wrietCards[i]} for the wonderful ${event}`);
+  }
+}
+>>>>>>> b3edd017351b7257fd545a88b36d5e3a86fd5ee2
+>>>>>>> 292c2b7471c575d018d7ed647435c354adabdc6d
